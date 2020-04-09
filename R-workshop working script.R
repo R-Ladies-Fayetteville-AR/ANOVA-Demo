3e454cf36c90ab449964a6062cdbf33270954df6
stephanielong12 <stephanielong12@users.noreply.github.com>
2020-04-09 17:10:22 -0500

    updates with an intro to programming in R section in the beginning

--- R-workshop working script.R
+++ R-workshop working script.R
@@ -1,6 +1,101 @@
 # Intro to R workshop working script
 
-#setup
+############# Programming in R intro #############
+
+## R in its most basic form can be used as a calculator:
+
+5 + 3 # addition
+
+5+3   # looks cluttered without spacing, right? right?!
+
+5 - 3 # subtraction
+
+5 * 3 # multiplication
+
+5 / 3 # division
+
+5 %% 3      # find the remainder
+
+3^2         # exponentiation
+
+sqrt(25)    # square root
+
+### you can use R to assign values to variables ###
+
+x = 5						# assigns 5 to x
+
+x								# checks the value of x
+
+y <- 4					# another was to assign 4 to y
+
+10 -> z					# yet one more way to assign 10 to z
+
+x4 = 6					# assigns 6 to the variable x4
+
+x * x4  				# a calculation performed on variables
+
+test_object             # checking the value of a non-existent variable throws an error
+
+test_object <- 1.5
+
+test_object_2 <- 2 
+
+test_object_2 + test_object # can add objects together
+
+###### you can also test logical operations ########
+
+z > x  # is z greater than x
+
+x < y  # is x less than y
+
+z >= x  # is z greater than or equal to x
+
+z == y  # is z equal to y
+
+z - 6 == y  # is the value of z - 6 equal to y
+
+z != y  # is z not equal to y
+
+########### objects in R are stored as certain data types #########
+
+# 1 numeric
+typeof(x)
+
+# 2 character
+l <- "this is a list"
+typeof(l)
+
+# 3 logical
+typeof(TRUE)
+
+# 4 list (contains multiple data types)
+my_list <-list("a" = 2.5, "b" = TRUE, "c" = 1:3)
+typeof(my_list)
+
+# these are all vectors (1 dimension objects)
+# there are also 2 dimensional objects
+
+# 1 matrix (contains rows and columns)
+
+my_matrix <- matrix(data = c(1, 2, 3,
+              4, 5, 6,
+              7, 8, 9), nrow = 3, ncol = 3)
+my_matrix
+str(my_matrix)
+
+# 2 data frame (contains observations (rows) and variables (columns))
+my_dataframe <- as.data.frame(my_matrix)
+str(my_dataframe)
+
+# what sets this apart from matrices is that you can call on variables by their name using "$"
+my_dataframe$V1
+
+
+
+
+
+
+######## disney movie data exploration setup ###########
 
 load(file = "Movie Data.Rdata")
 head(movie_data)
