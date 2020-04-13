8b4dcddf16b6aef5d83d5868f8fd84756d49c344
stephanielong12 <stephanielong12@users.noreply.github.com>
2020-04-13 15:03:02 -0500

    updated with comments

--- R-Workshop Script.R
+++ R-Workshop Script.R
@@ -251,40 +251,48 @@ large_sample_genre$adj_gross_billion <- large_sample_genre$inflation_adjusted_gr
 
 ##### Plot 1: simple boxplot
 
-plot1 <- ggplot(data = large_sample_genre) +
-  geom_boxplot(aes(x = genre, 
-                   y = total_gross_billion, 
-                   fill = genre)) +
-  theme(axis.text.x  = element_text(angle=75, vjust=0.5, size=9),
-        legend.position = "none",
-        plot.title = element_text(hjust = 0.5)) +
-  labs(x = "Genre Category", 
-       y = "Total Gross Revenue (Billions)",
-       title = "Movie Genre Total Gross Revenue")
+plot1 <- ggplot(data = large_sample_genre) + # call our data layer to the plot
+  geom_boxplot(aes(x = genre, # adding our boxplot data, this line adds our x-axis
+                   y = total_gross_billion, # add our y-axis
+                   fill = genre)) + # fill the boxes with color based on or genre groups
+  theme(axis.text.x  = element_text(angle=75, vjust=0.5, size=9), # adjust the x-axis text so that it's angled and larger
+        legend.position = "none", # remove the legend since you can see what groups the colors belong to
+        plot.title = element_text(hjust = 0.5)) + # adjust our title to be in the center
+  labs(x = "Genre Category", # re-title the x-axis label
+       y = "Total Gross Revenue (Billions)", # re-title the y-axis label
+       title = "Movie Genre Total Gross Revenue") # add the title
 plot1
 
+# save our plot as a jpeg image
 ggsave("Movie Genre Boxplot.jpeg", plot1)
 
 
 ##### Plot 2: two barplots together
 
-#loading in cleaned data for plot
+# loading in cleaned data for plot, this way it's already organized
 load("Plot 2 cleaned data.Rdata")
+str(plot_data2)
 
-plot2 <- ggplot(data = plot_data2, aes(x = genre, 
-                              y = MRevenue, 
-                              fill = Revenue_Type)) +
-  geom_bar(stat = "identity", position = "dodge") +
-  theme_bw() +
+# here we have mean gross revenue for both adjusted and non-adjusted for inflation for each genre
+# we also have the standard error for revenue type across genres
+# standard error is a measurement of how much a sample mean deviates from the population mean and is pretty standard for plotting errorbars
+
+# same general set-up
+plot2 <- ggplot(data = plot_data2, 
+                aes(x = genre,
+                    y = MRevenue, 
+                    fill = Revenue_Type)) + # our colors here will reflect adjusted or total revenue
+  geom_bar(stat = "identity") + # adding our bar graph by assigning the height of the bar to be our MRevenue
+  theme_bw() + # adding a new more plain theme as our background
   theme(axis.text.x  = element_text(angle=75, vjust=0.5, size=9),
         legend.position = "none") +
-  facet_wrap(~Revenue_Type ~.) +
+  facet_wrap(~Revenue_Type) + # adding a "facet" so that there are two graphs plotted side by side
   labs(x = "Genre Category", 
-       y = "Mean Gross Revenue (Billions)", 
-       fill = "Revenue Type") +
-  geom_errorbar(aes(ymin=MRevenue-SE, ymax=MRevenue+SE), width=.2, position=position_dodge(.9))
+       y = "Mean Gross Revenue (Billions)") +
+  geom_errorbar(aes(ymin=MRevenue-SE, ymax=MRevenue+SE), width=.2, position=position_dodge(.9)) #adding our errorbars as standard error
 plot2
 
+# save our plot
 ggsave("Comparing Mean Gross Revenue for Inflation Adjustment across Movie Genre.jpeg", plot2)
 
 
@@ -294,8 +302,8 @@ library("patchwork") # package for combining plots
 
 #pulling just the top 25 total grossing movies
 top_total_25 <- movie_data %>% 
-  arrange(desc(total_gross)) %>%
-  slice(1:25)
+  arrange(desc(total_gross)) %>% #arranges the data in descending order by "total_gross"
+  slice(1:25) # slices just the top 25
 
 #pulling just the top 25 grossing movies adjusted for inflation
 top_adj_25 <- movie_data %>% 
@@ -306,20 +314,23 @@ top_adj_25 <- movie_data %>%
 top_25 <- merge(top_total_25[,c(1,5)], top_adj_25[,c(1,6)], all = T, by = "movie_title")
 top_25_pivot <- pivot_longer(top_25, cols = c("total_gross", "inflation_adjusted_gross"), names_to = "Type", values_to = "Revenue")
 
-# plot 3A
+# we're going to make two separate graphs of our top 25 movies and then combined them
+
+# plot 3A: top 25 based on "total_gross"
 total <- ggplot(data = top_total_25, 
-                aes(x = reorder(movie_title, total_gross), y = (total_gross/1000000))) +
-  geom_bar(stat = "identity", position = "dodge", fill = "turquoise2") +
-  theme_bw() +
-  coord_flip() +
-  theme(axis.title.y = element_blank(), 
+                aes(x = reorder(movie_title, total_gross), # we can arrange our bars to be descending from greatest to least
+                    y = (total_gross/1000000))) + # we can divide the y axis so it's showing Millions instead of Billions
+  geom_bar(stat = "identity", position = "dodge", fill = "turquoise2") + # change the color of our bars with "fill"
+  theme_bw() + 
+  coord_flip() + # we can flip our x and y coordinates
+  theme(axis.title.y = element_blank(), # remove the y axis title since we all the movies are labeled
         legend.position = "none",
         plot.title = element_text(hjust = 0.5))+
-  labs(y = "Total Revenue (Millions)",
+  labs(y = "Total Revenue (Millions)", # make sure our labels go with our data
        title = "Top 25 Movies Based on Total Revenue")
 total
 
-# plot 3B
+# plot 3B: top 25 based on "inflation_adjusted_gross"
 adj <- ggplot(data = top_adj_25, 
               aes(x = reorder(movie_title, inflation_adjusted_gross), y = (inflation_adjusted_gross/1000000000))) +
   geom_bar(stat = "identity", position = "dodge", fill = "salmon") +
@@ -336,4 +347,5 @@ adj
 plot3 <- total | adj
 plot3
 
+#save our plot
 ggsave("Comparing Top 25 Grossing Movies Adjusting for Inflation.jpeg", plot3, width = 20, height = 11)
